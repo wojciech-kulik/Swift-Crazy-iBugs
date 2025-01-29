87a548877ba13a1d51a6d3a53c0c2a2546894d20
Wojciech Kulik <3128467+wojciech-kulik@users.noreply.github.com>
2025-01-29 20:35:18 +0100

    Added iOS RTF issue with scaling

---
+++ Foundation/RTF issue/Mac/RTF Issue/ContentView.swift
@@ -0,0 +1,96 @@
+//
+//  ContentView.swift
+//  RTF issue
+//
+//  Created by Wojciech Kulik on 29/01/2025.
+//
+
+import SwiftUI
+import UniformTypeIdentifiers
+
+struct ContentView: View {
+    let rtfData = #"""
+    {\rtf1\ansi\ansicpg1252\cocoartf2821
+    \cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fswiss\fcharset0 Helvetica-Bold;}
+    {\colortbl;\red255\green255\blue255;}
+    {\*\expandedcolortbl;;}
+    {\*\listtable{\list\listtemplateid1\listhybrid{\listlevel\levelnfc23\levelnfcn23\leveljc0\leveljcn0\levelfollow0\levelstartat1\levelspace360\levelindent0{\*\levelmarker \{disc\}}{\leveltext\leveltemplateid1\'01\uc0\u8226 ;}{\levelnumbers;}\fi-360\li720\lin720 }{\listname ;}\listid1}}
+    {\*\listoverridetable{\listoverride\listid1\listoverridecount0\ls1}}
+    \deftab720
+    \pard\pardeftab720\sa240\partightenfactor0
+    
+    \f0\fs20 \cf0 \expnd0\expndtw0\kerning0
+    Hi \
+    Thank you for your interest in working with me. Some of my pricing details are below:\
+    \pard\tx220\tx720\pardeftab720\li720\fi-720\partightenfactor0
+    \ls1\ilvl0
+    \f1\b \cf0 {\listtext    \uc0\u8226     }Pricing:
+    \f0\b0  Something}
+    """#.data(using: .utf8)!
+
+    var body: some View {
+        VStack {
+            Text("Expected Result:")
+            RichTextEditor(text: try! .init(
+                data: rtfData,
+                options: [.documentType: NSAttributedString.DocumentType.rtf],
+                documentAttributes: nil
+            ))
+            .disabled(true)
+
+            Button("Copy HTML to Clipboard") {
+                let html = (try! NSAttributedString(
+                    data: rtfData,
+                    options: [.documentType: NSAttributedString.DocumentType.rtf],
+                    documentAttributes: nil
+                )).html!
+
+                NSPasteboard.general.declareTypes([.html], owner: nil)
+                NSPasteboard.general.setData(html.data(using: .utf8), forType: .html)
+
+                print(html)
+            }.padding(.vertical)
+
+            Text("Paste Content Below:")
+
+            RichTextEditor()
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+struct RichTextEditor: NSViewRepresentable {
+    var text: NSAttributedString?
+
+    func makeNSView(context: Context) -> NSTextView {
+        let textView = NSTextView()
+        textView.isEditable = true
+        textView.isRichText = true
+        textView.backgroundColor = NSColor.lightGray
+        textView.font = NSFont.preferredFont(forTextStyle: .body)
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
+            print(text != nil ? "\n\nBEFORE PASTING:" : "\n\nAFTER PASTING:")
+            print(textView.attributedString().html!)
+        }
+
+
+        return textView
+    }
+
+    func updateNSView(_ textView: NSTextView, context: Context) {
+        if let text {
+            textView.textStorage?.setAttributedString(text)
+        }
+    }
+}
+
+extension NSAttributedString {
+    var html: String? {
+        (try? data(
+            from: NSRange(location: 0, length: length),
+            documentAttributes: [.documentType: NSAttributedString.DocumentType.html]
+        )).flatMap { String(data: $0, encoding: .utf8) }
+    }
+}
---
+++ Foundation/RTF issue/Mac/RTF Issue/RTF_IssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  RTF_IssueApp.swift
+//  RTF Issue
+//
+//  Created by Wojciech Kulik on 29/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct RTF_IssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
